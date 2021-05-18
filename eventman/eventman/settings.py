8b75cc3f6dd243986613ea9f7b5e00caa904a31e
AMAL <mailtoamaldevcd@gmail.com>
2021-05-18 12:21:39 +0530

    theme dashboard

--- eventman/eventman/settings.py
+++ eventman/eventman/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'eventso.apps.EventsoConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- eventman/eventso/views.py
+++ eventman/eventso/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return render(request,"eventso/index.html")
+
+def theme(request):
+    return render(request,"eventso/theme.html")
